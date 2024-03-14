f667cf885928d05ca8eb701c71b36b35cc988d4d
aleksandrachrust <ola.ch17@wp.pl>
2024-03-14 16:52:36 +0100

    Druga wersja: logowanie + stworzony Home

--- app/src/main/java/com/example/budgetbuddy/LoginActivity.kt
+++ app/src/main/java/com/example/budgetbuddy/LoginActivity.kt
@@ -1,20 +1,49 @@
 package com.example.budgetbuddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.budgetbuddy.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)//fggffgvbv
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.logbutton.setOnClickListener{
+            val email = binding.email.text.toString()
+            val password = binding.passw.text.toString()
+
+            if( email.isNotEmpty() && password.isNotEmpty() )
+            {
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
+                    if(it.isSuccessful)
+                    {
+                        val intent = Intent(this, Home::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                    {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "Empty files are not allowed", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
