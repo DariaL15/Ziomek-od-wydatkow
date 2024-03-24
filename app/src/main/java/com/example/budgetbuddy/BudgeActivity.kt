7eb13e8a5792d439c7c9631683b6b95e73ff26bd
OlgaKakhnovich <kahnovicola@gmail.com>
2024-03-24 15:24:45 +0100

    menu

--- app/src/main/java/com/example/budgetbuddy/BudgeActivity.kt
+++ app/src/main/java/com/example/budgetbuddy/BudgeActivity.kt
@@ -1,20 +1,38 @@
 package com.example.budgetbuddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.budgetbuddy.databinding.ActivityBudgeBinding
+import com.example.budgetbuddy.databinding.ActivityLoginBinding
 
 class BudgeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBudgeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_budge)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityBudgeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.budgetbutton.setOnClickListener{
+            val savings = binding.savings.toString()
+            val budget = binding.budget.toString()
+
+            if( savings.isNotEmpty() && budget.isNotEmpty() )
+            {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
+            else
+            {
+                Toast.makeText(this, "Wprowadź swój budget", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
--- app/src/main/java/com/example/budgetbuddy/Home.kt
+++ app/src/main/java/com/example/budgetbuddy/Home.kt
@@ -4,17 +4,71 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.view.MenuItem
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_menu, R.string.closw_menu)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if(savedInstanceState ==null)
+        {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem((R.id.nav_home))
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.nav_home->supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            R.id.nav_settings->supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment()).commit()
+            R.id.nav_savings->supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SavingsFragment()).commit()
+            R.id.nav_debt->supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, DebtFragment()).commit()
+            R.id.nav_analytics->supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AnalyticsFragment()).commit()
+            R.id.nav_logout->Toast.makeText(this, "Wylogowane", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed(){
+        super.onBackPressed()
+        if(drawerLayout. isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else
+        {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
 }
